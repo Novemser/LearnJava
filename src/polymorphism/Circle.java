96369e03a02f217fecb695a3186babe7617ac87e
Novemser <2072806652@qq.com>
2016-09-19 16:17:42 +0800

    Reflection

--- src/polymorphism/Circle.java
+++ src/polymorphism/Circle.java
@@ -4,6 +4,12 @@ package polymorphism;
  * Created by Novemser on 5/5/2016.
  */
 public class Circle extends Shape {
+
+    @Override
+    public String toString() {
+        return "{Name: Circle}";
+    }
+
     @Override
     public void draw() {
         System.out.println("Circle.draw");
