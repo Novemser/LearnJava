c41140edfb67956c49679e698e60073f823d1da8
Novemser <2072806652@qq.com>
2016-10-11 15:47:52 +0800

    Syntactic suger added

--- src/Exception/LoggingExceptions.java
+++ src/Exception/LoggingExceptions.java
@@ -2,6 +2,7 @@ package Exception;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Iterator;
 import java.util.logging.Logger;
 
 /**
@@ -11,29 +12,51 @@ import java.util.logging.Logger;
  * 2016/10/11
  */
 public class LoggingExceptions {
-    public void sweet(String...args) {
-        for (String s : args)
+    public void sweet(Object...args) {
+        for (Object s : args)
             System.out.println(s);
-
     }
 
     public static void main(String...args) {
-        try {
-            throw new LoggingException();
-        } catch (LoggingException e) {
-            System.err.println("Caught the " + e);
-        }
-//        new LoggingExceptions().sweet("23", "2332", "asdsad", "czxcczxc");
+//        try {
+//            throw new LoggingException();
+//        } catch (LoggingException e) {
+//            System.err.println("Caught the " + e);
+//        }
+        new LoggingExceptions().sweet(new IterableClass<Double>(), "2332", "asdsad", "czxcczxc");
 
-        try {
-            throw new LoggingException();
-        } catch (LoggingException e) {
-            System.err.println("Caught the " + e);
-        }
+//        try {
+//            throw new LoggingException();
+//        } catch (LoggingException e) {
+//            System.err.println("Caught the " + e);
+//        }
 
     }
 
 }
+
+class IterableClass<T> implements Iterable<T> {
+    @Override
+    public String toString() {
+        return getClass().getCanonicalName();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public T next() {
+                return null;
+            }
+        };
+    }
+}
+
 class LoggingException extends Exception {
     private static Logger logger =
             Logger.getLogger("LoggingExceptions");
